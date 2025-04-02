090bbcf4698f2b98a621c9d87045032054b2ffc1
Pierryjean <89036905+Pierryjean@users.noreply.github.com>
2025-04-02 14:19:15 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,21 @@
+#Use libraries as "BeautifulSoup" used to analyze and extract data of webpages
+# and "Requests" to make HTTP requests finding content of webpages.
+# I'm using  the module "zipfile" to work with zip archives and OS trying to interact with
+# the operational system.
 
+import requests
+from bs4 import BeautifulSoup
+import zipfile
+import os
+
+#Implementation to acces the website and realize the download of  files
+def web_scrape(link):
+  #Making syntax most correct possible
+  response = requests.get('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-dasociedade/atualizacao-do-rol-de-procedimentos')
+  #print(response.text) #Receiving the content of webpage
+  soup = BeautifulSoup(response.text, 'html.parser')
+  
+def compress_files( files_list, output_name):
+  with zipfile.ZipFile(output_name, 'w') as zipf:
+      for file in files_list:
+          zipf.write(file)
